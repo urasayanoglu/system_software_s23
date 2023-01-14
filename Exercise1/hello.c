77dd24f44b86c3587f30f92e6be25afa76c896df
Uras Ayanoglu <uras@ayanoglu.org>
2023-01-14 21:55:53 +0200

    Add  hello.c - Initial commit

---
+++ Exercise1/hello.c
@@ -0,0 +1,16 @@
+/*
+File:           Hello.c
+Author:         Uras Ayanoglu
+Description:    Printing out Hello World! to screen
+*/
+
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World!\n");
+
+    return(0);
+}
+
