b6a5f2b31a6f5b9954696b49de78e24006291c15
Uras Ayanoglu <uras@ayanoglu.org>
2023-01-23 13:59:58 +0200

    Add Exercise 2 Task 4c - Showing sum, average, difference, product, division and power of the integers

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -5,6 +5,7 @@ Description:    Tasks 3,4, and 5 for Exercise 2 of System Software course by San
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
 
@@ -21,14 +22,37 @@ int main(void){
 
     // 4b - Check the numbers for their values and print them.
     if (number1 == number2){
-        printf("Both numbers are the same and they are: %ld\n", number1);
+        printf("\nBoth numbers are the same and they are: %ld\n", number1);
 
     } else if (number1 >= number2){
-        printf("Number1: %ld is greater than Number2: %ld\n", number1, number2);
+        printf("\nNumber1: %ld is greater than Number2: %ld\n", number1, number2);
 
     } else {
-        printf("Number2: %ld is greater than Number1: %ld\n", number2, number1);
+        printf("\nNumber2: %ld is greater than Number1: %ld\n", number2, number1);
     }
+    
+    // 4c - Sum, Average, Difference, Product,Division, and Power of the numbers.
+    long int sum = number1 + number2;
+    printf("\nThe sum of Number1 + Number2 equals to --> %ld + %ld = %ld\n", number1, number2, sum);
+    
+    double average = (number1 + number2) / 2;
+    printf("The average of Number1 and Number2 equals to --> (%ld + %ld) / 2 = %.2f\n", number1, number2, average);
+
+    long int difference = number1 - number2;
+    printf("The difference of Number1 - Number2 equals to --> %ld - %ld = %ld\n", number1, number2, difference);
+
+    long int product = number1 * number2;
+    printf("The product of Number1 * Number2 equals to --> %ld * %ld = %ld\n", number1, number2, product);
+
+    if (number2 != 0){
+        float division = (float)number1 / (float)number2;
+        printf("The division of Number1 by Number2 equals --> %ld / %ld = %.2f\n", number1, number2, division);
+    } else {
+        printf("Zero Devision Error! You cannot divide a number by zero\n");
+    }
+
+    double power = pow(number1, number2);
+    printf("The Number1 to the power Number2 equals --> %ld ** %ld = %.2f\n", number1, number2, power);      
 
     return 0;
 }
