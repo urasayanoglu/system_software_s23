0ca328a1bb903954cf908998d3533ea6f070e860
Uras Ayanoglu <uras@ayanoglu.org>
2023-01-23 12:39:12 +0200

    Add Exercise 2 Task 4a - Reading two integers and printing them

---
+++ Exercise2/Exercise2.c
@@ -0,0 +1,20 @@
+/*
+File:           question4.c
+Author:         Uras Ayanoglu 
+Description:    Tasks 3,4, and 5 for Exercise 2 of System Software course by Sanna Määttä.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    // 4a - Read two integers from the user and print them.
+    int number1 = 0;
+    int number2 = 0;
+
+    printf("Enter an integer for Number1: ");
+    scanf("%d", &number1);
+
+    printf("Enter an integer for Number2: ");
+    scanf("%d", &number2);
+}
