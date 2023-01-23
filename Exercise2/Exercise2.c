06218e088d003d10a4d0b4ece18d0cb672545349
Uras Ayanoglu <uras@ayanoglu.org>
2023-01-23 12:51:52 +0200

    Add Exercise 2 Task 4b - Comparing integers

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -9,12 +9,26 @@ Description:    Tasks 3,4, and 5 for Exercise 2 of System Software course by San
 int main(void){
 
     // 4a - Read two integers from the user and print them.
-    int number1 = 0;
-    int number2 = 0;
+    long int number1 = 0;
+    long int number2 = 0;
 
     printf("Enter an integer for Number1: ");
-    scanf("%d", &number1);
+    scanf("%ld", &number1);
 
     printf("Enter an integer for Number2: ");
-    scanf("%d", &number2);
+    scanf("%ld", &number2);
+
+
+    // 4b - Check the numbers for their values and print them.
+    if (number1 == number2){
+        printf("Both numbers are the same and they are: %ld\n", number1);
+
+    } else if (number1 >= number2){
+        printf("Number1: %ld is greater than Number2: %ld\n", number1, number2);
+
+    } else {
+        printf("Number2: %ld is greater than Number1: %ld\n", number2, number1);
+    }
+
+    return 0;
 }
