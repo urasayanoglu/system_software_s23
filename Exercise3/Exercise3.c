57b58e0f1cfe178a853aee1fdffd659ff39fd7a2
Uras Ayanoglu <uras@ayanoglu.org>
2023-01-30 11:01:59 +0200

    Add Exercise3 Task 3b - Sum of even numbers between 1-1000

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -18,5 +18,15 @@ int main(void){
 
     printf("Sum of numbers between 1 - 100 is: %d", sumOfNumbers);
 
+    //3b - Count the sum of even numbers between 1-1000
+
+    int sumOfEvenNumbers = 0;
+
+    for (int i = 2; i < 1002; i=i+2){
+        sumOfEvenNumbers += i;
+    }
+
+    printf("\nSum of even numbers between 1 - 1000 is: %d", sumOfEvenNumbers);
+
     return 0;
 }
