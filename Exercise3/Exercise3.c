393513eace3035f0c6167757dd2e315f1575cc12
Uras Ayanoglu <uras@ayanoglu.org>
2023-02-04 12:00:09 +0200

    Add Exercise3 Task 4 - Collatz Sequence

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -11,7 +11,7 @@ Description:    Tasks 3,4, and 5 for Exercise 3 of System Software course by San
 #include <limits.h>
 
 int main(void){
-
+    printf("Tasks 3a, 3b and 3c:\n");
     //3a - Count the sum of numbers 1-100
     
     int sumOfNumbers = 0;
@@ -30,7 +30,7 @@ int main(void){
         sumOfEvenNumbers += i;
     }
 
-    printf("\nSum of even numbers between 1 - 1000 is: %d", sumOfEvenNumbers);
+    printf("\nSum of even numbers between 1 - 1000 is: %d\n", sumOfEvenNumbers);
 
     //3c - Generate random int. between 2 and 20, multiply it with itself before integer overflow.
     srand(time(NULL));
@@ -42,7 +42,7 @@ int main(void){
     int maxLimit = (int) sqrt((float)INT_MAX);        // Max integer value that the variable can hold
 
     do {
-        printf("\nInteger is: %d and its product by itself: %d", generatedNumber, generatedNumber*generatedNumber);
+        printf("Integer is: %d and its product by itself: %d\n", generatedNumber, generatedNumber*generatedNumber);
         generatedNumber *= generatedNumber;
     }
     while 
@@ -50,8 +50,33 @@ int main(void){
 
 
 
+    //4 - Generate a random number under 100 and generate Collatz sequence 
+    printf("\nTask 4: Collatz Sequence");
 
+    srand(time(NULL));
+    int collatz_upper = 100;
+    int collatz_lower = 1;
+    int collatzNumber = rand() % (collatz_upper - collatz_lower + 1) + lower;
+    printf("\nThe number generated for collatz sequence is: %d\n", collatzNumber);
+    
+    int counter = 1;
+
+    printf("Collatz sequence for number %d is: %d", collatzNumber, collatzNumber);
+    while (collatzNumber > 1){
+
+        if (collatzNumber % 2 == 0){
+            collatzNumber /= 2;
+            counter += 1;
+            printf(", %d", collatzNumber);
+        }else{
+            collatzNumber = collatzNumber * 3 + 1;
+            counter += 1;
+            printf(", %d", collatzNumber);
+        }
+    }
+    printf("\nLength of the collatz sequence is %d", counter);
 
 
     return 0;
+
 }
