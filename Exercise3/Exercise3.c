8b5ce8da694da3688950887fb89c639f787fb521
Jan-Krister Helenius <jan-krister.helenius@edu.turkuamk.fi>
2023-02-05 15:23:41 +0200

    Header

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -1,6 +1,6 @@
 /*
 File:           Exercise3.c
-Author:         Uras Ayanoglu 
+Author:         Uras Ayanoglu, Jan-Krister Helenius
 Description:    Tasks 3,4, and 5 for Exercise 3 of System Software course by Sanna Määttä.
 */
 
