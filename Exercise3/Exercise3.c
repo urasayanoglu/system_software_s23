6000d91bfe534f88cb7879ecfb60727a3b497d2a
Uras Ayanoglu <uras@ayanoglu.org>
2023-02-06 09:29:43 +0200

    Add Test cases for the Exercise3

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -39,17 +39,29 @@ int main(void){
     srand(time(NULL));
     int upper = 20;
     int lower = 2;
+    // int generatedNumber = -3;                   // Test case
     int generatedNumber = rand() % (upper - lower +1) + lower;
     printf("\nGenerated number between 2 and 20 is: %d\n", generatedNumber);
 
     int maxLimit = (int) sqrt((float)INT_MAX);        // Max integer value that the variable can hold
+    
+    if (generatedNumber == 1 || generatedNumber == 0) {
 
-    do {
+        printf("Integer is %d, and the sequence its squares and their squares is %d, %d, %d, ...\n", generatedNumber, generatedNumber, generatedNumber, generatedNumber);
+
+    }else if (generatedNumber == -1) {
+
+        printf("Integer is -1, and the sequence of its squares and their squares is 1, 1, 1, ...\n"); 
+    } else {
+        
+        do {
         printf("Integer is: %d and its product by itself: %d\n", generatedNumber, generatedNumber*generatedNumber);
         generatedNumber *= generatedNumber;
-    }
+        }
     while 
         (generatedNumber <= maxLimit); // Checks if the product is under the maxLimit that the variable can hold.
+
+    }
 
 
 
@@ -60,11 +72,23 @@ int main(void){
     int collatz_upper = 100;
     int collatz_lower = 1;
     int collatzNumber = rand() % (collatz_upper - collatz_lower + 1) + lower;
+    // int collatzNumber = 0;              // test case
     printf("\nThe number generated for collatz sequence is: %d\n", collatzNumber);
     
     int counter = 1;
 
+    if (collatzNumber < 1) {
+        
+        printf("There is no Collatz sequence for numbers smaller than 1.\n");
+
+    }else {
+        
+        printf("Collatz sequence for number %d is: %d\n", collatzNumber, collatzNumber);
+        
+        }
+
     printf("Collatz sequence for number %d is: %d", collatzNumber, collatzNumber);
+
     while (collatzNumber > 1){
 
         if (collatzNumber % 2 == 0){
@@ -77,14 +101,21 @@ int main(void){
             printf(", %d", collatzNumber);
         }
     }
-    printf("\nLength of the collatz sequence is %d\n", counter);
-    
+
+    if (collatzNumber > 0) {
+
+    printf("\nLength of the Collatz sequence is %d\n", counter);
+
+    }
 
     //5 - Generate a Fibonacci series and check if the random number is in the series.
     printf("\nTask 5 - Fibonacci Series:\n");
     
     srand(time(NULL));
     int fibonacciNumber = rand() % 100000;
+
+    // int fibonacciNumber = -1;            // test case
+
     printf("The number generated for Fibonacci series is: %d\n", fibonacciNumber);
     
     if (isFibonacci(fibonacciNumber)){
@@ -102,20 +133,20 @@ int main(void){
 
     int isFibonacci(int number){
 
-            int a = 0;
-            int b = 1;
+        int a = 0;
+        int b = 1;
 
-            while (a < number && b < number){
+        while (a < number && b < number){
 
-                if (a < b){
+            if (a < b){
 
-                    a = a + b;
+                a = a + b;
 
-                }else{
+            }else{
 
-                    b = a + b;
+                b = a + b;
 
-                }
             }
-            
-            return (a == number || b == number);}
+    }
+        
+        return (a == number || b == number || number == 0);}
