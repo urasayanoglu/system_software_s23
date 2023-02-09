667d5a20763f4465d5341c51f2ab1d99714a56a2
Uras Ayanoglu <uras@ayanoglu.org>
2023-02-09 14:15:33 +0200

    Add Exercise4 Task 4b - Generate a random number and check what grade it corresponds

--- Exercise4/Exercise4.c
+++ Exercise4/Exercise4.c
@@ -13,18 +13,36 @@ Description:    Tasks 3,4, and 5 for Exercise 3 of System Software course by San
 
 // Function declarations 
 int randNum(int x, int y);
+int gradeCheck(int points);
 
 int main(){
 
     // Task 4a - Write a function that generates a random number between the ranges of the function parameters.
     
-    int randomNumber = 0;
-
+    printf("Task 4a - Generating a random number between the ranges of the function parameters.\n");
+    
+    int randomNumber = 0;    
     randomNumber = randNum(150, -300);
     // randomNumber = randNum(0,0); // Test case1
     // randomNumber = randNum(-1,30); // Test case2
     // randomNumber = randNum(299, 300); // Test case3
     // randomNumber = randNum(160, 20); // Test case4
+
+    // Task 4b - Write a function that checks the corresponding grade of the random number generated in the previous task.
+    printf("\nTask 4b - Checking the corresponding grade of the random number generated in the previous task.\n");
+    int gradeNumber = 0;
+    gradeNumber = randNum(0, 120);
+    //gradeNumber = -20; // Test case1
+    // gradeNumber = 130; // Test case2
+    // gradeNumber = 0; // Test case3
+    // gradeNumber = 60; // Test case4
+    // gradeNumber = 72; // Test case5
+    // gradeNumber = 84; // Test case6
+    // gradeNumber = 96; // Test case7
+    // gradeNumber = 108; // Test case8
+    // gradeNumber = 120; // Test case9
+    gradeCheck(gradeNumber);
+
 
     return 0;
 }
@@ -55,4 +73,37 @@ int randNum(int x, int y)
     
     return randomNumber;
 
+}
+
+int gradeCheck(int points){
+
+    int grade = 0;
+
+    int grades[6] = {0,1,2,3,4,5};
+
+    if (points < 0 || points > 120){
+        printf("Points: %d\n", points);
+        printf("Invalid points. Grade points should be between 0 and 120.\n");
+    } else if (points < 60){
+        printf("%d points corresponds to grade %d.\n",points, grades[0]);
+        grade = grades[0];
+    } else if(points < 72){
+        printf("%d points corresponds to grade %d.\n", points, grades[1]);
+        grade = grades[1];
+    } else if (points < 84){
+        printf("%d points corresponds to grade %d.\n", points, grades[2]);
+        grade = grades[2];
+    } else if (points < 96){
+        printf("%d points corresponds to grade %d.\n", points, grades[3]);
+        grade = grades[3];
+    } else if (points < 108){
+        printf("%d points corresponds to grade %d.\n", points, grades[4]);
+        grade = grades[4];
+    } else if (points <= 120){
+        printf("%d points corresponds to grade %d.\n", points, grades[5]);
+        grade = grades[5];
+    }
+
+    return grade;
+    
 }
