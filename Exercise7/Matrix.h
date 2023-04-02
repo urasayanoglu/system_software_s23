afb35ae99fd4547fbf3b4746ce9b35472a4f9db4
Uras Ayanoglu <uras@ayanoglu.org>
2023-04-02 11:16:45 +0300

    Add Exercise7 Task 3 Find the greatest sum row,column,diagonal of the matrix

---
+++ Exercise7/Matrix.h
@@ -0,0 +1,23 @@
+/*
+File      :   Matrix.h
+Time      :   02.04.2023 22:42:46
+Compile   :   gcc -Wall -Wextra -std=c18 Matrix.h -o Matrix
+Version   :   1.0
+Author(s) :   Uras Ayanoglu, Jan-Krister Helenius
+Contact   :   uras.ayanoglu@edu.turkuamk.fi, jan-krister.helenius@edu.turkuamk.fi
+Desc      :   Header file for Matrix.c
+*/
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+// Determine the size of the array
+#define SIZE 20
+
+// Declare functions we wish to share for other files to use
+void testMatrix();
+int greatestSum();
+
+
+#endif 
+
